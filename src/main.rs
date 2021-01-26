a19e540d8c2d2003ab4d1149783d3ad1be7680f2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-01-25 19:22:32 -0600

    fail

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 use std::fmt::Display;
+use std::rc::Rc;
 use std::vec::Vec;
 
 // Lifetime annotations must precede generic types in the angle brackets.
@@ -10,7 +11,7 @@ struct Node<'a, T> {
 }
 
 impl<'a, T: Display> Node<'a, T> {
-    fn add_child(&mut self, child: &'a Node<'a, T>) {
+    fn add_child(&mut self, child: &'a Rc<Node<'a, T>>) {
         self.children.borrow_mut().push(child);
     }
 
@@ -27,13 +28,13 @@ impl<'a, T: Display> Node<'a, T> {
 }
 
 fn main() {
-    let mut a: Node<char> = Node::new('A');
-    let mut b: Node<char> = Node::new('B');
-    let c: Node<char> = Node::new('C');
-    let d: Node<char> = Node::new('D');
+    let mut a: Rc<Node<char>> = Rc::new(Node::new('A'));
+    let mut b: Rc<Node<char>> = Rc::new(Node::new('B'));
+    let c: Rc<Node<char>> = Rc::new(Node::new('C'));
+    let d: Rc<Node<char>> = Rc::new(Node::new('D'));
 
-    a.add_child(&b);
-    a.add_child(&c);
-    b.add_child(&d);
+    a.add_child(&Rc::clone(&b));
+    a.add_child(&Rc::clone(&c));
+    b.add_child(&Rc::clone(&d));
     a.depth_first();
 }
