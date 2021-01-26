127005576f58b1beb453f4ebc7d7846a6d92639a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-01-25 18:53:57 -0600

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
-#[derive(Debug)]
-struct Node<T> {
-    data: T
-}
-
-impl<T> Node<T> {
-    fn new(data: T) -> Node<T> {
-        Node { data }
-    }
-}
+use std::fmt::Display;
+use std::vec::Vec;
 
 // Lifetime annotations must precede generic types in the angle brackets.
 #[derive(Debug)]
-struct Edge<'a, T> {
-    from: &'a Node<T>,
-    to: &'a Node<T>,
+struct Node<'a, T> {
+    data: T,
+    children: Vec<&'a Node<'a, T>>
 }
-impl<'a, T> Edge<'a, T> {
-    fn new(from: &'a Node<T>, to: &'a Node<T>) -> Edge<'a, T> {
-        Edge { from, to }
+
+impl<'a, T: Display> Node<'a, T> {
+    fn add_child(&mut self, child: &'a Node<T>) {
+        self.children.push(child);
+    }
+
+    fn new(data: T) -> Node<'a, T> {
+        Node { data, children: Vec::new() }
+    }
+
+    fn depth_first(&self) {
+        println!("node {}", self.data);
+        for child in &self.children {
+            child.depth_first();
+        }
     }
 }
 
 fn main() {
-    let a: Node<char> = Node::new('A');
-    let b: Node<char> = Node::new('B');
+    let mut a: Node<char> = Node::new('A');
+    let mut b: Node<char> = Node::new('B');
     let c: Node<char> = Node::new('C');
     let d: Node<char> = Node::new('D');
-    let e1: Edge<char> = Edge::new(&a, &b);
-    let e2: Edge<char> = Edge::new(&a, &c);
-    let e3: Edge<char> = Edge::new(&b, &d);
-    dbg!(e1, e2, e3);
+
+    a.add_child(&b);
+    a.add_child(&c);
+    b.add_child(&d);
+    a.depth_first();
 }
